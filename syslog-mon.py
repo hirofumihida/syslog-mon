e585aa66d20312a6ca81d26172a06c9fe7d09beb
Hirofumi Hida <Hirofumi@Hirofumis-MacBook-Air.local>
2016-09-21 06:26:25 +0900

    multiple lines handling

--- syslog-mon.py
+++ syslog-mon.py
@@ -49,30 +49,36 @@ class Parser(object):
 
 def main():
   parser = Parser()
-
+  fields_list = []
   with open('/var/log/syslog') as syslogFile:
     for line in syslogFile:
       fields = parser.parse(line)
       if fields != None:
         if fields['appname'] == 'kernel' and 'md/raid' in fields['message']:
           #print "parsed:", fields
-          return fields
+          fields_list.append(fields)
+          #return fields
+    print fields_list
+    return fields_list
 
 if __name__ == "__main__":
-  content_dic = main()
-  if content_dic != None:
-    #print content_dic
-    content = content_dic['timestamp'] + " " + content_dic['hostname'] + " " + content_dic['appname'] + " " + content_dic['message']
-    #print content
+  content_list = main()
+  #print content_list
+  if len(content_list) != 0:
+    for i in range(len(content_list)):
+      #print content_list[i]
+      #print content_list
+      content = content_list[i]['timestamp'] + " " + content_list[i]['hostname'] + " " + content_list[i]['appname'] + " " + content_list[i]['message']
+      #print content
 
-    payload_dic = {
-        "text":content,
-        "username":'RAID Messages',
-        "icon_emoji":':warning:',
-        "channel":channel_name,
-        }
+      payload_dic = {
+          "text":content,
+          "username":'RAID Messages',
+          "icon_emoji":':warning:',
+          "channel":channel_name,
+          }
 
-    r = requests.post(url, data=json.dumps(payload_dic))
+      r = requests.post(url, data=json.dumps(payload_dic))
   else:
     exit()
 
